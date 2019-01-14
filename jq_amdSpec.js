c9660e90e7538ca36a569cb907786aebfbf5018e
David Shams <david.shams@gmail.com>
2019-01-14 11:07:26 -0600

    FAMOUS BUG:     ReferenceError: define is not defined

--- jq_amdSpec.js
+++ jq_amdSpec.js
@@ -17,8 +17,16 @@
 describe('jq_amd', function(){
     describe('add function', function(){
         it('has been called', function(){
+            define(['jq_amd'], [], function(jqamd){
+                return {
+                    add: function(){}
+                }
+            })
             require(['jq_amd'], function(jqamd){
-                expect(add).toHaveBeenCalled();
+                spyOn(jqamd, "add");
+                // jqamd.add(); // Call the function you spy on
+                expect(jqamd.add).toHaveBeenCalled();
+
             })
         })
     })
