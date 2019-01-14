6a1b6dd5d3502f7e3b37bf3c4a170bc430986834
David Shams <david.shams@gmail.com>
2019-01-13 18:55:58 -0600

    FAMOUS BUG: describe is not defined

--- karma.conf.js
+++ karma.conf.js
@@ -1,39 +1,56 @@
 module.exports = (config) => {
     config.set({
-      // ... normal karma configuration
-      files: [
-        // all files ending in "_test"
-        { pattern: 'test/*Spec.js', watched: false },
-        { pattern: 'test/**/*Spec.js', watched: false }
-        // each file acts as entry point for the webpack configuration
-      ],
-  
-      preprocessors: {
-        // add webpack as preprocessor
-        'test/*_test.js': [ 'webpack' ],
-        'test/**/*_test.js': [ 'webpack' ]
-      },
-      plugins: [
-        'karma-chrome-launcher',
-        // 'karma-tap',
-        // 'karma-sourcemap-loader',
-        'karma-webpack' // *** This 'registers' the Karma webpack plugin.
-      ],
-      
-  
-      webpack: {
-        // karma watches the test entry points
-        // (you don't need to specify the entry option)
-        // webpack watches dependencies
-  
-        // webpack configuration
-      },
-  
-      webpackMiddleware: {
-        // webpack-dev-middleware configuration
-        // i. e.
-        stats: 'errors-only'
-      }
-    })
-  }
+
+        // ... normal karma configuration
+        // base path that will be used to resolve all patterns (eg. files, exclude)
+        basePath: '',
+
+        reporters: ['progress'],
+        colors: true,
+        autoWatch: true,
+        browsers: ['Chrome'],
+
     
+
+
+        // frameworks to use
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        // frameworks: ['jasmine', 'requirejs'],
+
+
+
+        files: [
+            // all files ending in "_test"
+            { pattern: 'test/*Spec.js', watched: false },
+            { pattern: 'test/**/*Spec.js', watched: false }
+            // each file acts as entry point for the webpack configuration
+        ],
+
+        preprocessors: {
+            // add webpack as preprocessor
+            'test/*Spec.js': ['webpack'],
+            'test/**/*_Spec.js': ['webpack']
+        },
+        plugins: [
+            'karma-chrome-launcher',
+            // 'karma-tap',
+            // 'karma-sourcemap-loader',
+            'karma-webpack' // *** This 'registers' the Karma webpack plugin.
+        ],
+
+
+        webpack: {
+            // karma watches the test entry points
+            // (you don't need to specify the entry option)
+            // webpack watches dependencies
+
+            // webpack configuration
+        },
+
+        webpackMiddleware: {
+            // webpack-dev-middleware configuration
+            // i. e.
+            stats: 'errors-only'
+        }
+    })
+}
